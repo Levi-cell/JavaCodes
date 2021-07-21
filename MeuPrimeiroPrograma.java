dd1f7f944644873a3db7f88058347d88cd42f832
Levi-cell <levigov21@gmail.com>
2021-07-20 22:52:35 -0300

    project

---
+++ MeuPrimeiroPrograma.java
@@ -0,0 +1,14 @@
+public class MeuPrimeiroPrograma {
+
+    public static void main(String[] args) {
+        minhaInstrucaodeimprimir();
+
+    }
+    static void minhaInstrucaodeimprimir() {
+        System.out.println(2 + 2);
+        System.out.println("boa noite puta");
+        System.out.println(" vidinho soma "  + 16/2 );
+
+
+    }
+}
